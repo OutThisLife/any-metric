41f6e8be2e2a2f184f576c9345e87e272d7f6658
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-12-10 22:45:35 -0600

    quick gal commit

--- app/server/schema/mutations/deleteTag.ts
+++ app/server/schema/mutations/deleteTag.ts
@@ -1,9 +1,9 @@
-import { ObjectId } from 'bson'
+import * as mongoose from 'mongoose'
 
 import { Resolver } from '../types'
 
 export default (async (_, { input: { id } }, { mongo }) => {
   const col = await mongo.collection('tags')
-  const res = await col.deleteOne({ _id: new ObjectId(id) })
+  const res = await col.deleteOne({ _id: new mongoose.mongo.ObjectID(id) })
   return res.deletedCount === 1
 }) as Resolver

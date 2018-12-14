ea32ace01cfe81d350382a2e1cb46646cf8cfa4c
Talasan Nicholson <talasan.nicholson@gmail.com>
2018-12-14 02:06:06 -0600

    quick gal commit

--- app/components/Particles/index.tsx
+++ app/components/Particles/index.tsx
@@ -1,47 +1,60 @@
-import { MeasuredComponentProps, withContentRect } from 'react-measure'
 import Particles, { IParticlesParams } from 'react-particles-js'
-import { compose, setDisplayName, withPropsOnChange } from 'recompose'
+import {
+  branch,
+  compose,
+  renderComponent,
+  setDisplayName,
+  withHandlers,
+  withState
+} from 'recompose'
 
 import defaultParams from './params'
 
-export default compose<ParticleProps & MeasuredComponentProps, {}>(
+export default compose<ParticleProps, {}>(
   setDisplayName('particles'),
-  withContentRect('bounds'),
-  withPropsOnChange<ParticleProps, ParticleProps & MeasuredComponentProps>(
-    ['contentRect'],
-    ({ contentRect }) => {
-      const params = defaultParams
+  branch(() => !('browser' in process), renderComponent(() => null)),
+  withState('params', 'setParams', defaultParams),
+  withHandlers(() => ({
+    onRef: ({ params, setParams }) => ref => {
+      if (!ref) {
+        return
+      }
 
-      const b =
-        contentRect.bounds.width > 1025 &&
-        document.visibilityState === 'visible'
+      const el = ref as HTMLElement
 
-      params.particles.color.value = '#fafafa'
-      params.particles.line_linked.color = params.particles.color.value
-      params.interactivity.events.onhover.enable = b
-      params.particles.move.enable = b
+      el.style.pointerEvents = 'none'
+      el.style.zIndex = '100'
+      el.style.pointerEvents = 'none'
+      el.style.position = 'fixed'
+      el.style.top = '0'
+      el.style.left = '0'
+      el.style.width = '100%'
+      el.style.height = '100%'
+      ;(el.style as any).mixBlendMode = 'soft-light'
 
-      return { params }
+      const maybeFreeze = () => {
+        const b =
+          window.innerWidth > 1025 && document.visibilityState === 'visible'
+
+        params.particles.color.value = '#fafafa'
+        params.particles.line_linked.color = params.particles.color.value
+        params.interactivity.events.onhover.enable = b
+        params.particles.move.enable = b
+
+        window.requestAnimationFrame(() => setParams(params))
+      }
+
+      window.requestAnimationFrame(maybeFreeze)
+      document.addEventListener('visibilitychange', maybeFreeze)
+      window.addEventListener('resize', maybeFreeze)
     }
-  )
-)(({ measureRef, params, contentRect: { bounds: { width, height } } }) => (
-  <div
-    ref={measureRef}
-    id="particles"
-    style={{
-      zIndex: 100,
-      pointerEvents: 'none',
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      mixBlendMode: 'soft-light'
-    }}>
+  }))
+)(({ onRef, params }) => (
+  <div ref={onRef} id="particles">
     <Particles
       params={params}
-      width={width + 'px'}
-      height={height + 'px'}
+      width={window.innerWidth + 'px'}
+      height={window.innerHeight + 'px'}
       style={{
         opacity: 0.2
       }}
@@ -50,5 +63,8 @@ export default compose<ParticleProps & MeasuredComponentProps, {}>(
 ))
 
 interface ParticleProps {
+  onRef?: (r: HTMLElement) => void
+  freeze?: () => void
   params: Partial<IParticlesParams>
+  setParams: (p: ParticleProps['params']) => void
 }

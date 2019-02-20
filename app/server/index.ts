1669311b2fee2a72f16ab687c931c5ceb76e83d6
Talasan Nicholson <talasan.nicholson@gmail.com>
2019-02-20 00:26:05 -0600

    quick gal commit

--- app/server/index.ts
+++ app/server/index.ts
@@ -32,6 +32,7 @@ nextApp.prepare().then(() => {
     app
       .use(helmet())
       .use(morgan('combined', {}))
+      .enable('trust proxy')
       .use(
         compression({
           level: 6,
